12aaf720c2485c1ea9932079164dfc91db7cc616
harrisbang2 <harrisbang98@gmail.com>
2024-01-21 23:42:29 +0900

    http data to object

--- src/main/java/com/sparta/spring_mvc/Request/RequestController.java
+++ src/main/java/com/sparta/spring_mvc/Request/RequestController.java
@@ -31,7 +31,7 @@ public class RequestController {
     //  Content type: application/x-www-form-urlencoded
     // Body
     //  name=Robbie&age=95
-    @PostMapping("/form/param")
+    @PostMapping("/form/param")//
     @ResponseBody
     public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) {
         return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
